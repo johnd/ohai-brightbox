7bf47601481227b91dc6a92b981af3edc12c999c
John Daniels <john.daniels@itison.com>
2017-09-08 09:56:32 +0100

    Update plugin to use ohai 4+ syntax

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'john@semantici.st'
 license          'Apache 2.0'
 description      'Installs/Configures ohai-brightbox'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
 
-depends 'ohai'
+depends 'ohai', '>= 4.0.0'
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,16 +1,8 @@
 # Cookbook Name:: ohai-brightbox
 # Recipe:: default
 #
-# Copyright 2014, Itison, Ltd
+# Copyright 2017, Itison, Ltd
 
-ohai "reload brightbox" do
-  plugin "brightbox"
-  action :nothing
-end
-
-cookbook_file "#{node[:ohai][:plugin_path]}/brightbox.rb" do
-  source "brightbox.rb"
-  notifies :reload, "ohai[reload brightbox]"
-end
+ohai_plugin 'brightbox'
 
 include_recipe "ohai::default"
